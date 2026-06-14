c548a1a73f58cadfc2c35946f042f174230cb43e
ec1yps_ <ec1yps@yandex.ru>
2026-06-14 22:22:19 +0300

    feat: add user registration endpoint

---
+++ Planner_server/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Planner_server.Data;
+using Planner_server.DTOs;
+using Planner_server.Models;
+namespace Planner_server.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class AuthController : Controller
+	{
+		private readonly AppDbContext _context;
+		public AuthController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpPost("register")]
+		public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+		{
+			if (!ModelState.IsValid)
+				return BadRequest(ModelState);
+
+			bool usernameExists = await _context.Users
+				.AnyAsync(u => u.Username == request.Username);
+
+			if (usernameExists)
+				return BadRequest("Username already exists");
+
+			bool emailExists = await _context.Users
+				.AnyAsync(u => u.Email == request.Email);
+
+			if(emailExists)
+				return BadRequest("Email already exists");
+
+			User user = new User
+			{
+				Username = request.Username,
+				Email = request.Email,
+				PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password)
+			};
+
+			_context.Users.Add(user);
+
+			await _context.SaveChangesAsync();
+
+			return Ok("User registered successfully");
+		}
+	}
+}
--- Planner_server/Program.cs
+++ Planner_server/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Planner_server.Data;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Planner_server
 {
@@ -12,6 +15,25 @@ namespace Planner_server
 			// Add services to the container.
 			builder.Services.AddDbContext<AppDbContext>(options =>
 				options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
+
+			builder.Services.AddAuthentication(
+				JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options =>
+				{
+					options.TokenValidationParameters =
+					new TokenValidationParameters
+					{
+						ValidateIssuer = true,
+						ValidateAudience = true,
+						ValidateLifetime = true,
+						ValidateIssuerSigningKey = true,
+
+						ValidIssuer = builder.Configuration["Jwt:Issuer"],
+						ValidAudience = builder.Configuration["Jwt:Audience"],
+						IssuerSigningKey = new SymmetricSecurityKey(
+							Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+					};
+				});
 
 			builder.Services.AddControllers();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -29,8 +51,8 @@ namespace Planner_server
 
 			app.UseHttpsRedirection();
 
+			app.UseAuthentication();
 			app.UseAuthorization();
-
 
 			app.MapControllers();
 
