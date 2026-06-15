89a1994c4b9d805762b518e51ea39b78e1fa54ec
ec1yps_ <ec1yps@yandex.ru>
2026-06-15 23:20:43 +0300

    feat: add user login endpoint

--- Planner_server/Controllers/AuthController.cs
+++ Planner_server/Controllers/AuthController.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Planner_server.Data;
 using Planner_server.DTOs;
 using Planner_server.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
 namespace Planner_server.Controllers
 {
 	[ApiController]
@@ -10,9 +15,12 @@ namespace Planner_server.Controllers
 	public class AuthController : Controller
 	{
 		private readonly AppDbContext _context;
-		public AuthController(AppDbContext context)
+		private readonly IConfiguration _configuration;
+
+		public AuthController(AppDbContext context, IConfiguration configuration)
 		{
 			_context = context;
+			_configuration = configuration;
 		}
 
 		[HttpPost("register")]
@@ -30,10 +38,10 @@ namespace Planner_server.Controllers
 			bool emailExists = await _context.Users
 				.AnyAsync(u => u.Email == request.Email);
 
-			if(emailExists)
+			if (emailExists)
 				return BadRequest("Email already exists");
 
-			User user = new User
+			User user = new()
 			{
 				Username = request.Username,
 				Email = request.Email,
@@ -46,5 +54,58 @@ namespace Planner_server.Controllers
 
 			return Ok("User registered successfully");
 		}
+
+		[HttpPost("login")]
+		public async Task<ActionResult<AuthResponse>> Login(LoginRequest request)
+		{
+			var user = await _context.Users
+				.FirstOrDefaultAsync(
+				u => u.Username == request.Username);
+
+			if (user == null)
+				return Unauthorized("Invalid username");
+
+			bool validPassword = BCrypt.Net
+				.BCrypt.Verify(request.Password, user.PasswordHash);
+
+			if (!validPassword)
+				return Unauthorized("Invalid password");
+
+			string token = GenerateToken(user);
+
+			return Ok(new AuthResponse { Token = token });
+		}
+
+		private string GenerateToken(User user)
+		{
+			Claim[] _claims =
+			[
+				new Claim(
+				ClaimTypes.NameIdentifier,
+				user.Id.ToString()),
+
+				new Claim(
+					ClaimTypes.Name,
+					user.Username!)
+			];
+
+			SymmetricSecurityKey key = new(
+				Encoding.UTF8.GetBytes(
+					_configuration["Jwt:Key"]!));
+
+
+			SigningCredentials credentials = new(
+				key, SecurityAlgorithms.HmacSha256);
+
+			JwtSecurityToken token = new(
+				issuer: _configuration["Jwt:Issuer"],
+				audience: _configuration["Jwt:Audience"],
+				claims: _claims,
+				expires: DateTime.UtcNow.AddDays(7),
+				signingCredentials: credentials);
+
+			return new JwtSecurityTokenHandler().WriteToken(token);
+		}
+
 	}
 }
